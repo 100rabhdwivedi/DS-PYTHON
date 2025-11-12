37786ba42c497d4de8469a3c1b55dd5681ae84bc
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-12 21:07:23 +0530

    Operators

---
+++ class4.py
@@ -0,0 +1,46 @@
+#Operators
+
+#Arithmetic operators +,-,*,/,//,**,%  in py operators follow BODMAS rule
+
+print(10+12)
+print(30-20)
+print(30*20)
+print(10/2)
+print(10%2)
+
+#Floor division
+print(100//23.4)
+
+#Exponensial opeartor
+print(5**3)
+
+#Assignment operators +=,-=,/=,*=,//=,**=
+a=30
+print(a)
+
+a+=30
+print(a)
+
+a-=10
+print(a)
+
+a*=10
+print(a)
+
+a/=5
+print(a)
+
+a**=3
+print(a)
+
+a//=40
+print(a)
+
+#Concatination and multiplication on string
+
+a="hello"
+b=" world"
+
+print(a+b)
+print(f"a\n"*3)
+
