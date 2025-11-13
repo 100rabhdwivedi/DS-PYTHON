ae8d32ff3be3c6787f927ed8aeeced5b83c2faaf
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-13 20:02:32 +0530

    if else with pass

--- class6.py
+++ class6.py
@@ -1,6 +1,16 @@
+#1. if else 
 age = int(input("Enter your age:"))
 
 if age>=18:
     print("You are eligible for vote")
 else:
-    print("You are not eligible for vote")    
+    print("You are not eligible for vote")   
+
+
+# 2.if else with pass
+money = float(input("Enter a amount : "))
+
+if money >=2000:
+    pass
+else:
+    print("Money is lessthan 2000")
