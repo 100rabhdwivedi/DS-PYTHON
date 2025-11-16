f6949553c275730851cbf8ca48a7b9181714fbd9
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-16 21:24:17 +0530

    Solved a lots of for loop problems

--- class7Q.py
+++ class7Q.py
@@ -28,3 +28,43 @@ for i in range(1, n3 + 1):
 
 print("Even sum =", esum)
 print("Odd sum =", odsum)
+
+# 4.Find the factors of number
+
+num = int(input("Enter a number that's factor you want:"))
+
+for i in range (1,num+1):
+    if num%i ==0:
+        print(i)
+
+#5 sum of all factors
+num1 = int(input("Enter a number that's factor you want:"))
+sm=0
+for i in range (1,num1+1):
+    if num1%i ==0:
+        sm+=i
+print(f"Sum of factors: {sm}") 
+
+
+#6 power calculation
+
+a = int(input("Enter a number ")) 
+b = int(input("Enter a power"))  
+
+ans=1
+for i in range(1,b+1):
+    ans*=a
+
+print("Answer = ",ans)
+
+# 7.Prime number checker
+
+number1 = int(input("Enter a number "))
+
+for i in range (2,number1//2+1):
+    if number1%i == 0:
+        print("Composite number")    
+        break
+
+else:
+    print("Prime number")
