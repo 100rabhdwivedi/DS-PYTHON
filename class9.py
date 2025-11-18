1d9de55913e7b25be5ebaa1e19f7ffede3228df7
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-18 23:34:30 +0530

    Addition with the help of function:

--- class9.py
+++ class9.py
@@ -1,4 +1,9 @@
 #Defining and calling the function
 def greet():
-    print("Hello welcom:")
-greet()    
+    print("Hello welcome:")
+greet()  
+
+def addition (b,a=20):
+    return a+b
+
+print("Addition =",addition(30))
