69f854e404a4de15041819533b8543cb9f416fc8
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-19 23:38:14 +0530

    Check the number is positive,negative,zero

--- class9Q.py
+++ class9Q.py
@@ -4,3 +4,15 @@ def welcome_message(name):
 
 
 print(welcome_message("saurabh"))
+
+#Check the number is positive,negative,zero
+
+def check_number_sign(num):
+    # Write your code here
+    if num>0:
+        return "Positive"
+    elif num <0 :
+        return "Negative"
+    else :
+        return "Zero"
+print(check_number_sign(20))  
