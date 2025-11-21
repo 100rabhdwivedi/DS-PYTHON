7b7e6ae05e5e66a1a9edac434e106d7d1180409e
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-21 21:45:05 +0530

    About list

---
+++ class11.py
@@ -0,0 +1,16 @@
+#list is a hetrogenous nature 
+a = [12,13,"saurabh",{"xyz":"prq"},(1,2,3)]
+
+#list is used to store duplicate value
+
+b= [1,22,3,3,22,1,44,22,44]
+
+#list is mutable
+
+b[0]=10
+print(b)
+
+#list also support slicing
+print(b[:3])
+
+
