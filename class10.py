3cd733d040fba9ab7d1321ce23072737cf1c4008
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-21 21:20:49 +0530

    Learning about data structures

---
+++ class10.py
@@ -0,0 +1,13 @@
+"""
+some data structures 
+list,tuple,set,dictionary
+linkedlist,stack,queue,tree,graph
+
+"""
+
+list = [1,2,3,4]
+tuple = (1,3,2,4)
+set={1,2,3,4}
+dic={
+    "name":"saurabh"
+}
