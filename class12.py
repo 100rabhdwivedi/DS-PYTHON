41e0538c12abd164ac77b19f723feb5303ec47c9
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-23 23:38:18 +0530

    All the things completed about tuple

--- class12.py
+++ class12.py
@@ -15,3 +15,16 @@ tp1=(1,1,2,2,3,3)
 a,b,c=(10,20,30)
 
 print(c)
+
+#traversing on tuple
+#method1
+for i in tp:
+    print(i)
+#method2   
+for j in range(len(tp)):
+    print(tp[j]) 
+
+#tuple methods
+print('\n')
+print(tp1.count(3))
+print(tp1.index(3))
