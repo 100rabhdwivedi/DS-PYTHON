377ef599d3bab350d5944dc58f62486bfb6da44e
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-11-23 23:31:33 +0530

    About tuple

---
+++ class12.py
@@ -0,0 +1,17 @@
+#tuple has a hetrogenous nature
+
+tp = (1,2,"str1",3.0)
+
+#tuple also aloow to store duplicate value
+
+tp1=(1,1,2,2,3,3)
+
+#tuple is immutable
+
+# tp1[0] = 12 #not allowed
+
+#tuple unpacking
+
+a,b,c=(10,20,30)
+
+print(c)
