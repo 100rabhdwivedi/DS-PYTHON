ab7ac989e78a149dffddb61c0c492cb0dd84733e
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2026-01-29 10:40:47 +0530

    Lambda func , map, filter, zip

---
+++ class15.py
@@ -0,0 +1,36 @@
+'''
+Lambda function ,
+map , filter , zip
+'''
+
+#Creating a lambda function 
+
+# square = lambda a : a**2
+
+# print("Square =", square(10))
+
+#Using a map function 
+
+# lis = [1,2,3,4]
+
+# out = map(lambda lis : lis ** 2,lis)
+
+# print(list(out))
+
+# Finding a even numbers using filter function
+
+# lis = [1,2,3,4,5,6,7]
+
+# even = filter(lambda lis : lis % 2 == 0,lis)
+
+# print(list(even))
+
+#Use of zip function 
+
+# name = ['gaurav','sumit','anurag','xyz']
+
+# age = [22,34,50,12]
+
+# comb = list(zip(name,age))
+
+# print(comb)
