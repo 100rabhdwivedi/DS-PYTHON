a3038557cd515894897b715e7c297806c0876103
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2026-02-02 09:46:14 +0530

    Abstraction

---
+++ class26.py
@@ -0,0 +1,20 @@
+"""
+Abstraction
+"""
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @property
+    @abstractmethod
+    def species(self):
+        pass  # Abstract property, must be implemented by subclasses
+
+class Dog(Animal):
+    @property
+    def species(self):
+        return "Canine"
+
+# Instantiate the concrete subclass
+dog = Dog()
+print(dog.species)
